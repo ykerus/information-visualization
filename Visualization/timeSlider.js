79db39f255e66afcc0c4c7e13502055798ebb1ba
hannahlim95 <han__l@msn.com>
2020-03-13 11:13:26 +0100

    add to branch

---
+++ Visualization/timeSlider.js
@@ -0,0 +1,95 @@
+var ABSMAX, ABSMIN;
+
+function getSlider(props) {
+    return d3
+            .sliderBottom()
+            .min(props.showMin)
+            .max(props.showMax)
+            .ticks(5)
+            .tickFormat(d3.format('.0f'))
+            .default([props.pointMin, props.pointMax])
+            .fill('#2196f3');
+}//getSlider
+
+function updateSlider(selection, props, data) {
+    var prevMin = props.showMin,
+        changeSlider = false;
+    slider = getSlider(props)
+    slider
+        .on("onchange", val => {
+            d3.select('p#value-range').text(val.map(d3.format('.0f')).join(' - '));
+        })
+        .on('end', val => {
+            dataSelection = getPeriod(data, val[0], val[1]);
+            if (dataSelection.creation_year.length == 0) {
+                val[0] = props.pointMin;
+                val[1] = props.pointMax;
+                slider.value([props.pointMin, props.pointMax]);
+                selection.call(slider);
+            }//if
+            else {
+                genderBar(genderGroup, dataSelection);
+                cultureBar(cultureGroup, dataSelection);
+//                bubbleChart(bubbleChartGroup, dataSelection);
+                loadBackground(dataSelection);
+
+                if (Math.abs(val[1]-val[0]) <= 500 && Math.abs(props.showMax-props.showMin) > 1000) {
+                    changeSlider = true;
+                    props.showMin = val[0] - 50;
+                    props.showMax = val[1] + 50;
+                    if (props.showMin < ABSMIN) props.showMin=ABSMIN;
+                    if (props.showMax > ABSMAX) props.showMax=ABSMAX;
+                }//if
+                else if (Math.abs(props.showMax-props.showMin) < 1000 &&
+                         val[0] == props.showMin && val[1] == props.showMax) {
+                    changeSlider = true
+                    props.showMin = ABSMIN;
+                    props.showMax = ABSMAX;
+                }//else if
+
+                props.pointMin = val[0];
+                props.pointMax = val[1];
+
+                if (changeSlider) {
+
+                    selection.transition().attr('transform', 'translate(250,20)')
+                             .call(slider.value([prevMin, prevMin]).width(0));
+                    selection.transition().delay(200).remove();
+                    selection = d3.select("svg").append("g");
+                    setTimeout(() => {
+                        slider = updateSlider(selection, props, data);
+                        selection.attr('transform', 'translate(250,20)').call(slider.width(0));
+                        selection.transition().attr('transform', 'translate(30,20)')
+                                 .call(slider.width(width-2*30))
+                    }, 400);
+                    setTimeout(() => {
+                        selection.call(slider.value([val[0], val[1]]));
+                    }, 480);
+                }//if
+                else
+                    selection.call(slider);
+            }//else
+    });
+    return slider;
+}//updateSlider
+
+function timeSlider(selection, data) {
+    ABSMIN = d3.min(data.creation_year);
+    ABSMAX = d3.max(data.creation_year);
+    var props = { pointMin: ABSMIN, pointMax: ABSMAX,
+                  showMin:  ABSMIN, showMax:  ABSMAX }
+    var slider = updateSlider(selection, props, data);
+
+    selection
+        .attr('transform', 'translate(250,20)')
+        .call(slider.width(0))
+        .transition()
+        .attr('transform', 'translate(30,20)')
+        .call(slider.width(width-2*30));
+
+    d3.select('p#value-range').text(
+        slider
+          .value()
+          .map(d3.format('.0f'))
+          .join(' - '));
+}//timeSlider
---
+++ Visualization/visualization.js
@@ -0,0 +1,35 @@
+function getPeriod(d, from, to) {
+	newData = {}
+    for (var key in d)
+    	newData[key] = [];
+    for (var i=0; i<d.creation_year.length; i++)
+    	if (d.creation_year[i] >= from && d.creation_year[i] <= to)
+       		for (var key in d)
+            	newData[key].push(d[key][i])
+    return newData;
+}//getPeriod
+
+var width = 800,
+    height = 600,
+    barWidth = 60;
+
+var svgContainer = d3.select("body").append("svg")
+        .attr("width", width)
+        .attr("height", height);
+
+var sliderGroup = svgContainer.append("g");
+var genderGroup = svgContainer.append("g");
+var cultureGroup = svgContainer.append("g");
+//var bubbleChartGroup = svgContainer.append("g");
+var donutChartGroup = svgContainer.append("g");
+
+d3.json("data.json").then(function(data) {
+    data.creation_year = data.creation_year.map((x)=>parseInt(x));
+
+    timeSlider(sliderGroup, data);
+    genderBar(genderGroup, data);
+    cultureBar(cultureGroup, data);
+//    bubbleChart(bubbleChartGroup, data);
+    donutChart(donutChartGroup, data);
+		loadBackground(data)
+});
